e3917b50fca78f54ab3b70c8a7806f80ec73703e
vo3xel <gh@v3x.xyz>
2021-12-06 18:46:48 +0100

    csv writer finished

--- rewex.py
+++ rewex.py
@@ -1,26 +1,34 @@
 import argparse
 import os
 import csv
-from re import I
 import sys
 from decimal import Decimal
-import pylightxl as xl
-import shutil
 import datetime
 
-from pylightxl.pylightxl import Database
+BLOCKPIT_FIELD_NAMES =  [   'id', \
+                            'exchange_name', \
+                            'depot_name', \
+                            'transaction_date', \
+                            'buy_asset', \
+                            'buy_amount', \
+                            'sell_asset', \
+                            'sell_amount', \
+                            'fee_asset', \
+                            'fee_amount', \
+                            'transaction_type', \
+                            'note', \
+                            'linked_transaction' ]
 
-XLSX_TABLE_NAME = "Tabelle1"
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         usage="%(prog)s INPUT-FILE [OUTPUT-FILE]",
-        description="Converts Beaconchain validator CSV rewards exports to Blockpit.io Excel file."
+        description="Converts Beaconchain validator CSV rewards exports to Blockpit.io CSV file."
     )
     parser.add_argument('input_file', type=str, nargs=1,
                     help='CSV export file name from beaconcha.in')
     parser.add_argument("-o", "--output-file", type=str, nargs=1,
-        help='Output file name (Excel) for Blockpit.io')
+        help='Output file name (CSV) for Blockpit.io')
     parser.add_argument("-d", "--depot-name", type=str, nargs=1,
         help='Blockpit.io depot name', default='Validator')
     parser.add_argument(
@@ -39,74 +47,70 @@ def parse_input_file_name(filename: str) -> str:
         sys.exit("Error: INPUT-FILE is not a valid file.")
 
 def parse_output_file_name(filename: str) -> str:
-    if len(str(filename).rsplit('.', 1)) != 2 or filename.rsplit('.', 1)[1] != 'xlsx':
-        sys.exit("OUTPUT-FILE name is invalid. File name must end with \".xlsx\"")
+    if len(str(filename).rsplit('.', 1)) != 2 or filename.rsplit('.', 1)[1] != 'csv':
+        sys.exit("OUTPUT-FILE name is invalid. File name must end with \".csv\"")
     else:
         return filename
 
-def clearXLSX(db: Database) -> Database:
-    for i in range(2, 18):
-        for j in range(2,14):
-            db.ws(ws='Tabelle1').update_index(row=i, col=j, val="")
-    return db
-
-def CSV2XLSX(input_file: str,output_file: str, depot_name: str) -> None:
+# Blockpit CSV format: Date & Time: DD.MM.YYYY HH:MM:SS, Decimal Separator: dot "." and Column Separator: comma ","
+# More info: https://help.blockpit.io/hc/de-at/articles/360011877920
+def CSV2Blockpit(input_file: str,output_file: str, depot_name: str) -> None:
     with open(input_file, newline='') as csvfile:
-        shutil.copy(os.path.join(os.path.dirname(os.path.realpath(__file__)),'tpl','blockpit_xlsx_template.xlsx'),output_file)
-        db = xl.readxl(output_file)
-        db = clearXLSX(db)
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         headers = next(csvreader, None)
-        total_ETH_rewards = 0
-        total_EUR_rewards = 0
-        xlsx_transaction_id = 1
-        average_ETH_price = 0
-        for row in csvreader:
-            if xlsx_transaction_id == 1:
-                start_date = row[0]
-            end_date = row[0]
-            current_ETH_reward = Decimal("".join(d for d in row[2] if d.isdigit() or d == '.'))
-            current_EUR_reward = Decimal("".join(d for d in row[4] if d.isdigit() or d == '.'))
-            average_ETH_price = average_ETH_price + Decimal("".join(d for d in row[3] if d.isdigit() or d == '.'))
-            total_ETH_rewards = total_ETH_rewards + current_ETH_reward
-            total_EUR_rewards = total_EUR_rewards + current_EUR_reward
-            date_CSV = datetime.datetime.strptime(row[0], '%Y-%m-%d')
-            date_CSV = date_CSV.replace(hour=23,minute=59,second=59)
-            date_XLSX = date_CSV.strftime("%d/%m/%Y %H:%M:%S")
-            db.ws(ws=XLSX_TABLE_NAME).update_index(row=xlsx_transaction_id+1, col=1, val=xlsx_transaction_id)
-            db.ws(ws=XLSX_TABLE_NAME).update_index(row=xlsx_transaction_id+1, col=3, val=depot_name)
-            db.ws(ws=XLSX_TABLE_NAME).update_index(row=xlsx_transaction_id+1, col=4, val=date_XLSX)
-            db.ws(ws=XLSX_TABLE_NAME).update_index(row=xlsx_transaction_id+1, col=5, val="ETH")
-            db.ws(ws=XLSX_TABLE_NAME).update_index(row=xlsx_transaction_id+1, col=6, val=current_ETH_reward)
-            db.ws(ws=XLSX_TABLE_NAME).update_index(row=xlsx_transaction_id+1, col=9, val="ETH")
-            db.ws(ws=XLSX_TABLE_NAME).update_index(row=xlsx_transaction_id+1, col=11, val="masternode")
-            xlsx_transaction_id = xlsx_transaction_id + 1
-        xl.writexl(db=db, fn=output_file)
-        average_ETH_price =  average_ETH_price/(xlsx_transaction_id-1)
-        print("---   CSV information   ---")
-        print(f"\tHeader: {', '.join(headers)}")
-        print(f"\tTime range: from {start_date[-1]} to {end_date}")
-        print(f"\tNumber of entries: {xlsx_transaction_id-1}")          
-        print(f"\tTotal ETH rewards: {total_ETH_rewards}")
-        print(f"\tTotal rewards in EUR: {total_EUR_rewards}")
-        print(f"\tAverage ETH price: {average_ETH_price}")
-        print(f"\tDepot-name: {depot_name}")
-        print("--- END CSV information ---")
-        print("")
-        print("---   EXCEL information   ---")
-        print(f"\tHeader: {', '.join(db.ws(ws='Tabelle1').row(row=1))}")
-        print("--- END EXCEL information ---")
+        with open(output_file, 'w', newline='') as csvwriterfile:
+            csvwriter = csv.writer(csvwriterfile, delimiter=',')
+            csvwriter.writerow(BLOCKPIT_FIELD_NAMES)
+            total_ETH_rewards = 0
+            total_EUR_rewards = 0
+            blockpit_transaction_id = 1
+            average_ETH_price = 0
+            for row in csvreader:
+                if blockpit_transaction_id == 1:
+                    start_date = row[0]
+                end_date = row[0]
+                current_ETH_reward = Decimal("".join(d for d in row[2] if d.isdigit() or d == '.'))
+                current_EUR_reward = Decimal("".join(d for d in row[4] if d.isdigit() or d == '.'))
+                average_ETH_price = average_ETH_price + Decimal("".join(d for d in row[3] if d.isdigit() or d == '.'))
+                total_ETH_rewards = total_ETH_rewards + current_ETH_reward
+                total_EUR_rewards = total_EUR_rewards + current_EUR_reward
+                date_CSV = datetime.datetime.strptime(row[0], '%Y-%m-%d')
+                date_CSV = date_CSV.replace(hour=23,minute=59,second=59)
+                date_Blockpit_CSV = date_CSV.strftime("%d.%m.%Y %H:%M:%S")
+                writerow = [blockpit_transaction_id,"exchange",depot_name,date_Blockpit_CSV,"ETH",current_ETH_reward,"","","ETH",0.0,"masternode"]
+                csvwriter.writerow(writerow)
+                #db.ws(ws=XLSX_TABLE_NAME).update_index(row=blockpit_transaction_id+1, col=1, val=blockpit_transaction_id)
+                #db.ws(ws=XLSX_TABLE_NAME).update_index(row=blockpit_transaction_id+1, col=3, val=depot_name)
+                #db.ws(ws=XLSX_TABLE_NAME).update_index(row=blockpit_transaction_id+1, col=4, val=date_XLSX)
+                #db.ws(ws=XLSX_TABLE_NAME).update_index(row=blockpit_transaction_id+1, col=5, val="ETH")
+                #db.ws(ws=XLSX_TABLE_NAME).update_index(row=blockpit_transaction_id+1, col=6, val=current_ETH_reward)
+                #db.ws(ws=XLSX_TABLE_NAME).update_index(row=blockpit_transaction_id+1, col=9, val="ETH")
+                #db.ws(ws=XLSX_TABLE_NAME).update_index(row=blockpit_transaction_id+1, col=11, val="masternode")
+                blockpit_transaction_id = blockpit_transaction_id + 1
+            average_ETH_price =  average_ETH_price/(blockpit_transaction_id-1)
+            print("---   CSV information   ---")
+            print(f"\tHeader: {', '.join(headers)}")
+            print(f"\tTime range: from {start_date[-1]} to {end_date}")
+            print(f"\tNumber of entries: {blockpit_transaction_id-1}")          
+            print(f"\tTotal ETH rewards: {total_ETH_rewards}")
+            print(f"\tTotal rewards in EUR: {total_EUR_rewards}")
+            print(f"\tAverage ETH price: {average_ETH_price}")
+            print(f"\tDepot-name: {depot_name}")
+            print("--- END CSV information ---")
+            print("")
+            print(f"Blockpit CSV written {output_file}")
     return
 
 def main() -> None:
     parser = init_argparse()
     args = parser.parse_args()
     input_file_name = parse_input_file_name(args.input_file[0])
-    if args.output_file is None:
-        output_file_name = input_file_name.rsplit('.',1)[0] + ".xlsx"
+    if args.output_file is None or args.input_file[0] == args.output_file[0]:
+        output_file_name = input_file_name.rsplit('.',1)[0] + "_blockpit.csv"
     else:
         output_file_name = parse_output_file_name(args.output_file[0])
     print(f"Input file: {input_file_name}")
     print(f"Output file: {output_file_name}")
-    CSV2XLSX(input_file_name,output_file_name,depot_name=args.depot_name)
+    CSV2Blockpit(input_file_name,output_file_name,depot_name=args.depot_name)
+
 main()
