f203829bd6fa4a392d0608bd24ac3234955a3055
vo3xel <gh@v3x.xyz>
2021-12-06 18:58:05 +0100

    tests added

---
+++ test_conversion.py
@@ -0,0 +1,9 @@
+import subprocess
+import filecmp
+
+def test_conversion():
+    process = subprocess.Popen(['python', 'rewex.py', './test_files/input.csv','-o','./test_files/test_output.csv'],
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    assert filecmp.cmp('./test_files/test_output.csv', './test_files/output.csv')
