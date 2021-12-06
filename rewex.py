6ed7d2a1a216aaf4a96b9769b8fd02f9aab7d8f5
vo3xel <gh@v3x.xyz>
2021-12-06 17:29:45 +0100

    xlsx read implemented

--- rewex.py
+++ rewex.py
@@ -1,8 +1,13 @@
 import argparse
 import os
 import csv
+from re import I
 import sys
 from decimal import Decimal
+import pylightxl as xl
+import shutil
+
+from pylightxl.pylightxl import Database
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -12,7 +17,9 @@ def init_argparse() -> argparse.ArgumentParser:
     parser.add_argument('input_file', type=str, nargs=1,
                     help='CSV export file name from beaconcha.in')
     parser.add_argument("-o", "--output-file", type=str, nargs=1,
-        help='Output file name (Excel) for Blockpit.io')                   
+        help='Output file name (Excel) for Blockpit.io')
+    parser.add_argument("-d", "--depot-name", type=str, nargs=1,
+        help='Blockpit.io depot name', default='Validator')
     parser.add_argument(
         "-v", "--version", action="version",
         version = f"{parser.prog} version 0.0.1"
@@ -34,36 +41,50 @@ def parse_output_file_name(filename: str) -> str:
     else:
         return filename
 
-def CSV2XLSX(input_file: str,output_file: str) -> None:
+def clearXLSX(db: Database) -> Database:
+    for i in range(2, 18):
+        for j in range(2,14):
+            db.ws(ws='Tabelle1').update_index(row=i, col=j, val="")
+    return db
+
+def CSV2XLSX(input_file: str,output_file: str, depot_name: str) -> None:
     with open(input_file, newline='') as csvfile:
+        shutil.copy(os.path.join(os.path.dirname(os.path.realpath(__file__)),'tpl','blockpit_xlsx_template.xlsx'),output_file)
+        db = xl.readxl(output_file)
+        db = clearXLSX(db)
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         headers = next(csvreader, None)
-        print(f"CSV header: {', '.join(headers)}")
-        number_of_entries = 0
         total_ETH_rewards = 0
         total_EUR_rewards = 0
-        dates = []
-        incomes_ETH = []
-        incomes_EUR = []
-        ETH_prices = []
+        xlsx_transaction_id = 1
+        average_ETH_price = 0
         for row in csvreader:
-            dates.append(row[0])
+            if xlsx_transaction_id == 1:
+                start_date = row[0]
+            end_date = row[0]
             current_ETH_reward = Decimal("".join(d for d in row[2] if d.isdigit() or d == '.'))
             current_EUR_reward = Decimal("".join(d for d in row[4] if d.isdigit() or d == '.'))
-            incomes_ETH.append(current_ETH_reward)
-            incomes_EUR.append(current_EUR_reward)
-            ETH_prices.append(Decimal("".join(d for d in row[3] if d.isdigit() or d == '.')))
-            number_of_entries = number_of_entries + 1
+            average_ETH_price = average_ETH_price + Decimal("".join(d for d in row[3] if d.isdigit() or d == '.'))
             total_ETH_rewards = total_ETH_rewards + current_ETH_reward
             total_EUR_rewards = total_EUR_rewards + current_EUR_reward
-        average_ETH_price =  sum(ETH_prices)/len(ETH_prices)
+            db.ws(ws='Tabelle1').update_index(row=xlsx_transaction_id+1, col=1, val=xlsx_transaction_id)
+            db.ws(ws='Tabelle1').update_index(row=xlsx_transaction_id+1, col=3, val=depot_name)
+            xlsx_transaction_id = xlsx_transaction_id + 1
+        xl.writexl(db=db, fn=output_file)
+        average_ETH_price =  average_ETH_price/(xlsx_transaction_id-1)
         print("---   CSV information   ---")
-        print(f"\tTime range: from {dates[-1]} to {dates[0]}")
-        print(f"\tNumber of entries: {number_of_entries}")          
+        print(f"\tHeader: {', '.join(headers)}")
+        print(f"\tTime range: from {start_date[-1]} to {end_date}")
+        print(f"\tNumber of entries: {xlsx_transaction_id-1}")          
         print(f"\tTotal ETH rewards: {total_ETH_rewards}")
         print(f"\tTotal rewards in EUR: {total_EUR_rewards}")
         print(f"\tAverage ETH price: {average_ETH_price}")
+        print(f"\tDepot-name: {depot_name}")
         print("--- END CSV information ---")
+        print("")
+        print("---   EXCEL information   ---")
+        print(f"\tHeader: {', '.join(db.ws(ws='Tabelle1').row(row=1))}")
+        print("--- END EXCEL information ---")
     return
 
 def main() -> None:
@@ -76,6 +97,5 @@ def main() -> None:
         output_file_name = parse_output_file_name(args.output_file[0])
     print(f"Input file: {input_file_name}")
     print(f"Output file: {output_file_name}")
-    CSV2XLSX(input_file_name,output_file_name)
-
+    CSV2XLSX(input_file_name,output_file_name,depot_name=args.depot_name)
 main()
